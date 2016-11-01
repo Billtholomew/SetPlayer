Fixes bug where normalization messed up images - Originally, vertices didn't line up correctly due to how they were ordered (and Delaunay relies on correct ordering). Now I base the order of vertices on their theta from the center of mass of the two shapes to be transformed between.
* Other fixes include:
- Auto canny added so that find_card_color is no longer needed
- PEP 8 standardization
- More code clean-up

* Infill is still wonky, and code is still in "DEBUG MODE"

python/CardAttributes.py
@@ -17,9 +17,7 @@ class AbstractAttribute:
     def find_contours(self, card_image):
         card_image_mask = (255 - ip.im_mask(card_image))
         contours, _ = ip.cv2.findContours(card_image_mask, ip.cv2.RETR_EXTERNAL, ip.cv2.CHAIN_APPROX_SIMPLE)
-        card_area = card_image.shape[0] * card_image.shape[1]
-        contours = [contour for contour in contours
-                    if (card_area / 20) < ip.cv2.contourArea(contour)]
+        contours = filter(lambda contour: (card_image_mask.size / 20) < ip.cv2.contourArea(contour), contours)
         self.contours = contours
 
     def __eq__(self, other):
@@ -47,8 +45,11 @@ class Infill(AbstractAttribute):
     def parse_image(self, card_image):
         self.find_contours(card_image)
         card_image_mask = np.zeros((card_image.shape[0], card_image.shape[1]), dtype=np.uint8)
-        card_image_mask = ip.cv2.erode(card_image_mask, np.ones((15, 15), np.uint8), iterations=1)
         ip.cv2.drawContours(card_image_mask, self.contours, -1, 255, -1)
+        edges = ip.auto_canny(ip.bgr2gray(card_image))
+        #ip.cv2.imshow("TEST", edges)
+        #ip.cv2.waitKey(0)
+        card_image_mask = ip.cv2.erode(card_image_mask, np.ones((15, 15), np.uint8), iterations=1)
         color, std_dev = ip.cv2.meanStdDev(ip.bgr2gray(card_image), mask=card_image_mask.astype("uint8"))
         color = color[0][0]
         std_dev = std_dev[0][0]

python/SetPlayer.py
@@ -9,32 +9,18 @@ from Card import Card
 from CardAttributes import Count, Color, Shape, Infill
 
 
-def find_card_color(im):
-    bg = np.hstack((im[[0, -1], :].flatten(), im[:, [0, -1]].flatten()))
-    background_color_mu, background_color_std = np.mean(bg), np.std(bg)
-    histogram = np.histogram(im,bins=16)
-    card_color_mu = None
-    for count, value in zip(histogram[0], histogram[1]):
-        if count > (im.size/8) and not (abs(value - background_color_mu) < 32):
-            card_color_mu = value
-            break
-    card_color_stddev = max(32, abs(card_color_mu - background_color_mu) - background_color_std * 1.5)
-    card_color_stddev = min(32, card_color_stddev)
-    return card_color_mu, card_color_stddev
-
-
 def find_cards_in_image(im):
     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    color_mu, color_std = find_card_color(im)
-    im = ip.threshold_image(im, color_mu, color_std)
-    contours, _ = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = [contour for contour in contours
-                if cv2.contourArea(contour) > (im.shape[0] * im.shape[1]) / 15 / 1000 * 1000 / 3]
-    trapezoids = map(transformation.simple_trapezoid, contours)
-    return trapezoids
+    edges = ip.auto_canny(im)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = map(lambda contour: cv2.approxPolyDP(contour, 0.05 * cv2.arcLength(contour, True), True), contours)
+    trapezoids = filter(lambda contour: len(contour) == 4, contours)
+    # To assume a contour is a card, it must be less than 10% the size of the image (no fewer than 12 cards allowed)
+    # and must be more than 2% of the image (no more than 24 cards allowed, with the whole set taking 1/2 the image)
+    return filter(lambda t: edges.size / 50 < cv2.contourArea(t) < edges.size / 10, trapezoids)
 
 
-def parse_image(card_image, cid, source_vertices):
+def parse_card_image(card_image, cid, source_vertices):
     card = Card(cid, source_vertices)
     count = Count(card_image)
     card.update_attribute(count)
@@ -51,10 +37,10 @@ def get_card_features(target_dimensions, im):
     card_borders = find_cards_in_image(im)
     transformer = transformation.Transformer(target_image_dimensions=target_dimensions)
     cards = {}
-    for cid, source_vertices in enumerate(card_borders):
-        card_image = transformer.transform(im, source_vertices)
-        border = np.fliplr(source_vertices).reshape((-1, 1, 2))
-        card = parse_image(card_image, cid, border)
+
+    for cid, border in enumerate(card_borders):
+        card_image = transformer.transform(im, border.copy())
+        card = parse_card_image(card_image, cid, border)
         cards[cid] = card
     return cards
 
@@ -62,9 +48,9 @@ def get_card_features(target_dimensions, im):
 # board is the group of cards that sets should be found in
 def generate_valid_sets(board, n=3):
     # iterate through all combinations of n SET cards
-    # note that this only creates combinations of cids
-    for cids in combinations(board.keys(),n):
-        cards = [board[c] for c in cids]
+    # note that this only creates combinations of card_ids
+    for card_ids in combinations(board.keys(), n):
+        cards = [board[card_id] for card_id in card_ids]
         # assume all cards will have the same attributes, so just pull from first card
         attributes = cards[0].attributes.keys()
         valid_set = True
@@ -72,38 +58,36 @@ def generate_valid_sets(board, n=3):
             # get the values for the current attribute for all cards
             # there should be 1 unique value (all same) or n unique values (all different)
             # if not, break early and set valid_set to False
-            a_vals = set(map(lambda x: x.attributes[a], cards))
-            if not (len(a_vals) == 1 or len(a_vals) == n):
+            a_values = set(map(lambda x: x.attributes[a], cards))
+            if not (len(a_values) == 1 or len(a_values) == n):
                 valid_set = False
                 break
         if valid_set:
-            yield sorted(cids)
+            yield sorted(card_ids)
 
 
 def visualize_set(card_set, im):
     nim = im.copy()
     color = (255, 0, 0)
-    cv2.drawContours(nim, map(lambda card: card.loc, card_set), -1, color, 3)
-    print map(lambda card: card.attributes['count'].data, card_set), \
-        map(lambda card: card.attributes['count'].classification, card_set)
-    ip.im_show(nim, "SET", 0)
-    pass
+    cv2.drawContours(nim, np.fliplr(map(lambda card: card.loc, card_set)), -1, color, 3)
+    cv2.imshow("SET", nim)
+    cv2.waitKey(0)
+
 
 fName = "../data/setTest.jpg"
-oim = ip.im_read(fName)
+oim = cv2.imread(fName, cv2.CV_LOAD_IMAGE_COLOR)
 all_cards = get_card_features(target_dimensions=(int(270), int(420), 3), im=oim)
 
 
-for card in all_cards.values():
-    print card.attributes['infill'].data[0]
-for card in all_cards.values():
-    print card.attributes['infill'].data[1]
+for c in all_cards.values():
+    print c.attributes['infill'].data[0]
+for c in all_cards.values():
+    print c.attributes['infill'].data[1]
 
 
 learning.classify_attributes(all_cards, ['shape', 'color', 'count', 'infill'])
 
 for cid_set in generate_valid_sets(all_cards, n=3):
-    card_set = map(lambda cid: all_cards[cid], cid_set)
-    visualize_set(card_set, oim)
+    visualize_set(map(lambda cid: all_cards[cid], cid_set), oim)
 
-ip.close_window()
+cv2.destroyAllWindows()

python/image_processing.py
@@ -1,6 +1,17 @@
 import numpy as np
 import cv2
 
+
+# auto canny function
+# from http://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/
+def auto_canny(im, sigma=0.33):
+    v = np.median(im)
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    return cv2.Canny(im, lower, upper)
+
+
 def bgr2gray(im):
     return cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
@@ -25,8 +36,8 @@ def threshold_image(im, color_mu, color_std=0, sigma=1):
     nim = im.copy()
     _, ima = cv2.threshold(im, color_mu - (color_std * sigma), 255, cv2.THRESH_BINARY)
     _, imb = cv2.threshold(im, color_mu + (color_std * sigma), 255, cv2.THRESH_BINARY_INV)
-    nim[np.equal(ima,imb)] = 255
-    nim[np.not_equal(ima,imb)] = 0
+    nim[np.equal(ima, imb)] = 255
+    nim[np.not_equal(ima, imb)] = 0
     nim = im_open(nim)
     nim = im_open(nim)
     return nim
@@ -36,25 +47,3 @@ def im_mask(im, sigma=1):
     im_gray = bgr2gray(im)
     card_color_mu, card_color_std = cv2.meanStdDev(im_gray)
     return threshold_image(im_gray, card_color_mu, card_color_std).astype("uint8")
-
-
-def im_read(fName, color='COLOR'):
-    if color == 'ANY_DEPTH':
-        color = cv2.CV_LOAD_IMAGE_ANYDEPTH
-    elif color == 'COLOR':
-        color = cv2.CV_LOAD_IMAGE_COLOR
-    elif color == 'GRAYSCALE':
-        color = cv2.CV_LOAD_IMAGE_GRAYSCALE
-    return cv2.imread(fName, color)
-
-
-def im_show(im, window_name='image', wait_time=0):
-    cv2.imshow(window_name, im)
-    cv2.waitKey(wait_time)
-
-
-def close_window(window_name=None, close_all_windows=False):
-    if close_all_windows:
-        cv2.destroyAllWindows()
-    else:
-        cv2.destroyWindow(window_name)

python/transformation.py
@@ -8,13 +8,16 @@ class Transformer:
         self.target_image_dimensions = target_image_dimensions
         # if target vertices are not set, assume we want to fill the entire targetImage
         if not target_vertices:
+            rows, columns = target_image_dimensions[:2]
             self.target_vertices = np.array([[0, 0],
-                                             [0, target_image_dimensions[1]],
-                                             [target_image_dimensions[0], target_image_dimensions[1]],
-                                             [target_image_dimensions[0], 0]])
+                                             [0, columns],
+                                             [rows, 0],
+                                             [rows, columns]])
         else:
             self.target_vertices = target_vertices
 
+        r0, c0 = np.round(np.mean(self.target_vertices, axis=0)).astype(np.int32)
+        self.target_vertices = np.array(sorted(self.target_vertices, key=lambda (r, c): np.arctan2(r0 - r, c0 - c)))
         # get barycentric coordinates and corresponding points in target (new) image
         nys = np.arange(self.target_image_dimensions[0])
         nxs = np.arange(self.target_image_dimensions[1])
@@ -38,21 +41,14 @@ class Transformer:
 
     def transform(self, oim, source_vertices):
         nim = np.zeros(self.target_image_dimensions, np.uint8)
+
+        source_vertices = source_vertices.reshape((-1, 2))
+        source_vertices = np.fliplr(source_vertices)
+        r0, c0 = np.round(np.mean(source_vertices, axis=0)).astype(np.int32)
+        source_vertices = np.array(sorted(source_vertices, key=lambda (r, c): np.arctan2(r0 - r, c0 - c)))
+
         rpts = source_vertices[self.indices, :]
         original_row = np.multiply(rpts[:, :, 0], self.barycentric).sum(axis=1, keepdims=True).astype("int32")
         original_col = np.multiply(rpts[:, :, 1], self.barycentric).sum(axis=1, keepdims=True).astype("int32")
         nim[self.targetRow, self.targetCol, :] = oim[original_row, original_col, :].reshape((-1, 3))
         return nim
-
-
-# given a roughly trapezoidal contour, turn it into a simple trapezoid with only 4 vertices
-def simple_trapezoid(contour):
-    contour = np.reshape(contour, (len(contour), 2))
-    rectangle = np.zeros((4, 2), dtype="int32")
-    s = contour.sum(axis=1)
-    rectangle[0] = contour[np.argmin(s)]
-    rectangle[2] = contour[np.argmax(s)]
-    diff = np.diff(contour, axis=1)
-    rectangle[1] = contour[np.argmin(diff)]
-    rectangle[3] = contour[np.argmax(diff)]
-    return np.fliplr(rectangle)